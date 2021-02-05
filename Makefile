1bd71666ffa7be4730e09aacbc9b61297899187d
Joseph D. Marhee <jmarhee@interiorae.com>
2021-02-04 21:44:59 -0600

    Updates push and release scripts

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ dist:
 
 .SILENT:
 tag-release:
-	if [[ $(TAG) == v?.?.? ]]; then echo "Tagging $(TAG)"; elif [[ $(TAG) == v?.?.?? ]]; then echo "Tagging $(TAG)"; else echo "Bad Tag Format: $(TAG)"; exit 1; fi && git tag -a $(TAG) -m "Releasing $(TAG)" ; read -p "Push tag: $(TAG)? " push_tag ; if [ "${push_tag}"="yes" ]; then git push origin $(TAG); fi
+	if [[ $(TAG) == v?.?.? ]]; then echo "Tagging $(TAG)"; elif [[ $(TAG) == v?.?.?? ]]; then echo "Tagging $(TAG)"; else echo "Bad Tag Format: $(TAG)"; exit 1; fi && git tag -a $(TAG) -m "Releasing $(TAG)" ; read -p "Push tag: $(TAG)? " push_tag ; if [ "${push_tag}"="yes" ]; then git push github $(TAG); fi
 
 create-release:
 	if [[ $(TAG) == v?.?.? ]]; then echo "Cutting release from $(TAG)"; elif [[ $(TAG) == v?.?.?? ]]; then echo "Cutting release from $(TAG)"; else echo "Bad Tag Format, cannot cut release: $(TAG)"; exit 1; fi && git tag -a $(TAG) -m "Releasing $(TAG)" ; read -p "Cut release from tag: $(TAG)? " push_tag ; if [ "${push_tag}"="yes" ]; then TAG=$(TAG) ./make-release.sh; fi
--- make-release.sh
+++ make-release.sh
@@ -3,8 +3,14 @@
 TAG=$TAG
 TAG_COMMIT=$(git rev-parse --short ${TAG})
 
-AUTH_TOKEN=$GITEA_TOKEN
-curl -X POST "https://git-central.openfunction.co/api/v1/repos/jmarhee/stripe-invoice-bot/releases" -H "Authorization: token $AUTH_TOKEN" \
-    -H  "accept: application/json" -H  "Content-Type: application/json" \
-    -d "{  \"body\": \"Cutting ${TAG} at ${TAG_COMMIT}\",  \"draft\": false,  \"name\": \"${TAG}\",  \"prerelease\": false,  \"tag_name\": \"${TAG}\",  \"target_commitish\": \"${TAG_COMMIT}\"}" \
-    -ik
+#AUTH_TOKEN=$GITEA_TOKEN
+#curl -X POST "https://git-central.openfunction.co/api/v1/repos/jmarhee/stripe-invoice-bot/releases" -H "Authorization: token $AUTH_TOKEN" \
+#    -H  "accept: application/json" -H  "Content-Type: application/json" \
+#    -d "{  \"body\": \"Cutting ${TAG} at ${TAG_COMMIT}\",  \"draft\": false,  \"name\": \"${TAG}\",  \"prerelease\": false,  \"tag_name\": \"${TAG}\",  \"target_commitish\": \"${TAG_COMMIT}\"}" \
+#    -ik
+
+AUTH_TOKEN=$GITHUB_TOKEN
+curl \
+  -X POST "https://api.github.com/repos/jmarhee/stripe-invoice-bot/releases" -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github.v3+json" \
+  -d "{  \"body\": \"Cutting ${TAG} at ${TAG_COMMIT}\",  \"draft\": false,  \"name\": \"${TAG}\",  \"prerelease\": false,  \"tag_name\": \"${TAG}\" }" \
